2e2e344c04d9852954bb4a022bfc0f77a492e2bc
Hoff22 <abithoff@gmail.com>
2023-06-12 22:53:49 -0300

    gps tracker

--- lab1_ex3/src/main/java/com/example/lab1_ex3/MainActivity_Ex3.java
+++ lab1_ex3/src/main/java/com/example/lab1_ex3/MainActivity_Ex3.java
@@ -1,20 +1,27 @@
 package com.example.lab1_ex3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity_Ex3 extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometer, mGiro, mIllum;
     private TextView textX, textY, textZ, g1, g2, g3, i1;
+    private Button locationButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +39,27 @@ public class MainActivity_Ex3 extends AppCompatActivity implements SensorEventLi
         g2 = findViewById(R.id.g2);
         g3 = findViewById(R.id.g3);
         i1 = findViewById(R.id.i1);
+        locationButt = findViewById(R.id.gpsButt);
+
+        locationButt = (Button) findViewById(R.id.gpsButt);
+        ActivityCompat.requestPermissions(MainActivity_Ex3.this, new
+                String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        locationButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GPSTracker g = new GPSTracker(getApplicationContext());
+                Location l = g.getLocation();
+                if(l!=null)
+                {
+                    double lat = l.getLatitude();
+                    double longi = l.getLongitude();
+                    Toast.makeText(getApplicationContext(), "LAT: "+lat + "LONG: " +
+                            longi, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
     @Override
     protected void onResume() {
         super.onResume();
